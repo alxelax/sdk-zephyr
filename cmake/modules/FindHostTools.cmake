80394df6c202b309a234ecad3152f7a5a479e757
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2026-04-08 11:05:48 +0200

    cmake: use zephyr 1.0 toolchain
    Optional commit to use Zephyr sdk toolchain 1.0.
    It can be reverted if not needed.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- cmake/modules/FindHostTools.cmake
+++ cmake/modules/FindHostTools.cmake
@@ -50,7 +50,7 @@ endif()
 
 find_package(Deprecated COMPONENTS CROSS_COMPILE)
 
-find_package(Zephyr-sdk 0.16)
+find_package(Zephyr-sdk 1.0)
 
 # gperf is an optional dependency
 find_program(GPERF gperf)
